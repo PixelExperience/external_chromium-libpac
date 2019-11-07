LineageOS/android_external_chromium-libpac

Fix use-after-free in proxy resolver
Bug: 139806216
Test: m -j proxy_resolver_v8_unittest && adb sync && adb shell \
      /data/nativetest64/proxy_resolver_v8_unittest/proxy_resolver_v8_unittest

Change-Id: I663829a8a7467f50f9b11fd8b787108ff5d64acc

test/proxy_resolver_v8_unittest.cc
@@ -643,5 +643,20 @@ TEST(ProxyResolverV8Test, B_132073833) {
   EXPECT_EQ("DIRECT", proxies[0]);
 }
 
+TEST(ProxyResolverV8Test, B_139806216) {
+  ProxyResolverV8WithMockBindings resolver(new MockJSBindings());
+  int result = resolver.SetPacScript(SCRIPT(B_139806216_JS));
+  EXPECT_EQ(OK, result);
+
+  // Execute FindProxyForURL().
+  result = resolver.GetProxyForURL(kQueryUrl, kQueryHost, &kResults);
+
+  EXPECT_EQ(OK, result);
+  std::vector<std::string> proxies = string16ToProxyList(kResults);
+  EXPECT_EQ(1U, proxies.size());
+  EXPECT_EQ("DIRECT", proxies[0]);
+}
+
+
 }  // namespace
 }  // namespace net

PixelExperience/external_chromium-libpac

Merged-In: I663829a8a7467f50f9b11fd8b787108ff5d64acc
(cherry picked from commit ed9838b89ee2c43a5240f411d9c77558b4b34966)

test/proxy_resolver_v8_unittest.cc
@@ -572,5 +572,20 @@ TEST(ProxyResolverV8Test, B_132073833) {
   EXPECT_EQ("DIRECT", proxies[0]);
 }
 
+TEST(ProxyResolverV8Test, B_139806216) {
+  ProxyResolverV8WithMockBindings resolver(new MockJSBindings());
+  int result = resolver.SetPacScript(String16(B_139806216_JS));
+  EXPECT_EQ(OK, result);
+
+  // Execute FindProxyForURL().
+  result = resolver.GetProxyForURL(kQueryUrl, kQueryHost, &kResults);
+
+  EXPECT_EQ(OK, result);
+  std::vector<std::string> proxies = string16ToProxyList(kResults);
+  EXPECT_EQ(1U, proxies.size());
+  EXPECT_EQ("DIRECT", proxies[0]);
+}
+
+
 }  // namespace
 }  // namespace net